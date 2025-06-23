6e4415304930cc070bdce03850ce2c0fc46f09ad
Mary ☆ <165522131+maryfreitz@users.noreply.github.com>
2025-06-23 19:48:10 -0300

    mudando a lógica do update

--- Psicólogo/PlanilhaPsicologo/update.php
+++ Psicólogo/PlanilhaPsicologo/update.php
@@ -1,17 +1,21 @@
 <?php
     include "../conexao.php";
 
+
     $cartao = $_POST["cartaoSUS"] ?? '';
     $nome = $_POST["nomePaciente"] ?? '';
     $medicamentos = $_POST["medicamentos"] ?? '';
+    $id = $_POST["id"] ?? '';
 
 
     if ($cartao && $nome && $medicamentos) {
-        $stmt = $conexao->prepare("UPDATE medicamento_psicologo SET cartaoSUS = :cartao, nome_paciente = :nome, medicamentos = :medicamentos WHERE medicamentos = :medicamentos");
+        $stmt = $conexao->prepare("UPDATE medicamento_psicologo SET cartaoSUS = :cartao, nome_paciente = :nome, medicamentos = :medicamentos WHERE id = :id");
 
         $stmt->bindValue(":cartao", $cartao);
         $stmt->bindValue(":nome", $nome);
         $stmt->bindValue(":medicamentos", $medicamentos);
+        $stmt->bindValue(":id", $id);
+        
 
         if ($stmt->execute()) {
             header("Location: ../../");
