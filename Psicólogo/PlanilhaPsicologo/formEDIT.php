50eed4f19ed34b45d8bcfeadeab3dc6421e3fafc
Mary ☆ <165522131+maryfreitz@users.noreply.github.com>
2025-06-23 19:52:19 -0300

    acrescentando a consulta no BD para ocupar os campos com as informações antigas.

--- Psicólogo/PlanilhaPsicologo/formEDIT.php
+++ Psicólogo/PlanilhaPsicologo/formEDIT.php
@@ -9,19 +9,29 @@
 <body>
     <img src="logo/ChatGPT_Image_15_de_mai._de_2025__10_39_50-removebg-preview (1)" id="logo-salus">
     <div id="caixa-formulario">
+        <?php 
+            $id = $_POST["id"];
+            require_once "../conexao.php";
 
+            $stmt = $conexao->prepare("SELECT * FROM medicamento_psicologo WHERE id = :id;");
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+            $medicamento = $stmt->fetch();
+        ?>
         <form action="update.php" id="formulario-cadastro" method="POST">
 
+            <input type="hidden" name="id" value="<?php echo $id?>">
+
             <label for="cpf">Cartão do SUS do Paciente:</label>
-            <input type="number" id="cpf" name="cartaoSUS" class="campo-texto"<?php echo $_POST["cartaoSUS"]?? ''?>required>
+            <input type="number" id="cpf" name="cartaoSUS" class="campo-texto" value="<?php echo $medicamento["cartaoSUS"]?>" required>
 
             <label for="nome">Nome do Paciente:</label>
-            <input type="text" id="nome" name="nomePaciente" class="campo-texto"<?php echo $_POST["nomePaciente"]?? ''?>required>
+            <input type="text" id="nome" name="nomePaciente" class="campo-texto" value="<?php echo $medicamento["nome_paciente"]?>" required>
 
             <label for="medicamentos">Medicamentos:</label>
-            <textarea type="text" id="medicamentos" name="medicamentos" class="campo-area"<?php echo $_POST["medicamentos"]?? ''?>required></textarea>
+            <input type="text" id="medicamentos" name="medicamentos" class="campo-area" value="<?php echo $medicamento["medicamentos"]?>" required>
 
-            <button type="submit" id="botao-enviar">Enviar</button>
+            <button type="submit" id="botao-enviar">Atualizar</button>
         </form>
     </div>
 
