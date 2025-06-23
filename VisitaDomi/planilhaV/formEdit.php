357c7e6e56afb2ea4922a6051de9f4bb38f29a43
Mary ☆ <165522131+maryfreitz@users.noreply.github.com>
2025-06-23 20:00:58 -0300

    acrescentando a consulta no BD para ocupar os campos com as informações antigas.

--- VisitaDomi/planilhaV/formEdit.php
+++ VisitaDomi/planilhaV/formEdit.php
@@ -10,32 +10,41 @@
 	<div id="pagina">
 		<img src="logo/ChatGPT_Image_15_de_mai._de_2025__10_39_50-removebg-preview (1)" alt="Logo Salus" id="logo">
 		<div id="formulario">
+        <?php 
+            $id = $_POST["id_domicilio"]?? '';
+            require_once "conexao.php";
+
+            $stmt = $conexao->prepare("SELECT * FROM visita_domiciliar WHERE id_domicilio = :id");
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+            $visitado = $stmt->fetch();
+        ?>
 			<form action="update.php" method="POST">
 				<div class="coluna-esquerda">
-				<label for="nome-visitado">Id do Visitado:</label>
-				<input type="text" id="id-visitado" name="idVisitado" class="campo-texto"<?php echo $_POST["idVisitado"]?? ''?>required>
+
+				<input type="hidden" name="idVisitado" value="<?php echo $id?? ''?>">
 
 				<label for="nome-visitado">Nome do Visitado:</label>
-				<input type="text" id="nome-visitado" name="nomeVisitado" class="campo-texto"<?php echo $_POST["nomeVisitado"]?? ''?>required>
+				<input type="text" id="nome-visitado" name="nomeVisitado" class="campo-texto"value="<?php echo $visitado["nome"]?? ''?>"required>
 
 				<label for="idade-visitado">Idade do Visitado:</label>
-				<input type="text" id="idade-visitado" name="idadeVisitado" class="campo-texto"<?php echo $_POST["idadeVisitado"]?? ''?>required>
+				<input type="text" id="idade-visitado" name="idadeVisitado" class="campo-texto"value="<?php echo $visitado["idade"]?? ''?>"required>
 
 				<label for="entregas">Entregas feitas ao Visitado:</label>
-				<input type="text" id="entregas" name="entregas" class="campo-texto"<?php echo $_POST["entregas"]?? ''?>required>
+				<input type="text" id="entregas" name="entregas" class="campo-texto"value="<?php echo $visitado["entregas"]?? ''?>"required>
 
 				<label for="data-visita">Data da Visita:</label>
-				<input type="date" id="data-visita" name="data" class="campo-texto"<?php echo $_POST["data"]?? ''?>required>
+				<input type="date" id="data-visita" name="data" class="campo-texto"value="<?php echo $visitado["datas"]?? ''?>"required>
 
 				<label for="agente">Agente Responsável:</label>
-				<input type="text" id="agente" name="agenteRespon" class="campo-texto"<?php echo $_POST["agenteRespon"]?? ''?>required>
+				<input type="text" id="agente" name="agenteRespon" class="campo-texto"value="<?php echo $visitado["agente"]?? ''?>"required>
 			</div>
 
 			<div class="coluna-direita">
 				<label for="motivo">Motivo da Visita:</label>
-				<textarea id="motivo" name="motivoVisita" class="campo-texto-area"></textarea>
+				<input id="motivo" name="motivoVisita" class="campo-texto-area" value="<?php echo $visitado["motivo_visita"]?? ''?>"required>
 
-				<button id="botao-enviar">Atualizar</button>
+				<button type="submit" id="botao-enviar">Atualizar</button>
 			</div>	
 			</form>
 
